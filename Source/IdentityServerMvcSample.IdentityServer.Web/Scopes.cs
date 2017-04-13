03ca27bd72074b2eab7800fbc787be81c79bb16f
devdigital <devdigital.cloud@gmail.com>
2017-04-13 16:20:57 +0100

    Initial commit

---
+++ Source/IdentityServerMvcSample.IdentityServer.Web/Scopes.cs
@@ -0,0 +1,14 @@
+﻿namespace IdentityServerMvcSample.IdentityServer.Web
+{
+    using System.Collections.Generic;
+
+    using IdentityServer3.Core.Models;
+
+    public static class Scopes
+    {
+        public static List<Scope> Get()
+        {
+            return new List<Scope> { new Scope { Name = "api1" } };
+        }
+    }
+}
---
+++ Source/IdentityServerMvcSample.IdentityServer.Web/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(IdentityServerMvcSample.IdentityServer.Web.Startup))]
+
+namespace IdentityServerMvcSample.IdentityServer.Web
+{
+    using System.Collections.Generic;
+
+    using IdentityServer3.Core.Configuration;
+    using IdentityServer3.Core.Services.InMemory;
+
+    using Serilog;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo
+                .LiterateConsole(outputTemplate: "{Timestamp:HH:mm} [{Level}] ({Name:l}){NewLine} {Message}{NewLine}{Exception}")
+                .CreateLogger();
+
+            var options = new IdentityServerOptions
+            {
+                Factory = new IdentityServerServiceFactory()
+                    .UseInMemoryClients(Clients.Get())
+                    .UseInMemoryScopes(Scopes.Get())
+                    .UseInMemoryUsers(new List<InMemoryUser>()),
+
+                RequireSsl = false
+            };
+
+            app.UseIdentityServer(options);
+        }
+    }
+}
